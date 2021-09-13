f1374d46d64e358a38621d270070a64c345f6801
hasnain <hasn0039@stud.kea.dk>
2021-09-13 08:44:54 +0200

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello, World!");
+        System.out.println("linje1\n"+"linje2\n");
+        System.out.println("""
+                Linje 1
+                Linje 2
+                LInje 3
+                """);
+
+    }
+}
